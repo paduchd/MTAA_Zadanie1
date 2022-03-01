ba69c37f3a4e128d3166742cc04f4412b743c256
paduchd <71753856+paduchd@users.noreply.github.com>
2022-03-01 21:20:41 +0100

    Add files via upload

---
+++ Program/main.py
@@ -0,0 +1,12 @@
+#   MTAA - Zadanie 1
+#   Meno: Denis Padúch
+#   AIS ID: 110866
+#   Cvičenie: Štvrtok 10:00 (Ing. Adam Ševčík)
+
+import sipfullproxy
+
+def main():
+    sipfullproxy.initSIP()
+
+
+main()
